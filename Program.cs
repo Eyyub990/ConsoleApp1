0fab478cb4d52343f984ed06d83147877488f866
Eyyub990 <eyyubga@code.edu.az>
2023-11-25 06:28:31 +0400

    task 2 etdim

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Task 1
+string name = "Eyyub";
+string surname = "Akbarov";
+string fullname = " ";
+fullname = name + " " + surname;
+Console.WriteLine(fullname);
+
+// Task 2
+int a = 3;
+int b = 5;
+Console.WriteLine(a + b);
+
+// Task 3
+int c = 3;
+int d = 5;
+Console.WriteLine(c - d);
+
+// Task 4
+int e = 2;
+int f = 3;
+int g = e * f;
+Console.WriteLine(g * g);
+
+//Task 5
+float h = 18;
+float i = 5;
+float j = h / i;
+Console.WriteLine(j);
